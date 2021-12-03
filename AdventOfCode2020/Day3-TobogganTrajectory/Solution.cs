176f9dbb7d5c6fa8169be22b7758b830b96a4299
dheerajdhall <ddhall@predictiveindex.com>
2021-12-02 23:46:17 -0500

    Completed Day 3 of Advent of code 2020

---
+++ AdventOfCode2020/Day3-TobogganTrajectory/Solution.cs
@@ -0,0 +1,46 @@
+﻿
+var map = File.ReadAllLines("input.txt");
+
+
+int depth = map.Length;
+
+
+int GetNumberOfTreesAlongSlope(int horizontalMovement, int verticalMovement)
+{
+    int numberOfTrees = 0;
+
+    int currentHorizontalPos = 0;
+    int currentVerticalPos = 0;
+
+    while (currentVerticalPos < depth)
+    {
+        if (map.ElementAt(currentVerticalPos).ElementAt(currentHorizontalPos) == '#')
+        {
+            numberOfTrees++;
+        }
+
+        currentHorizontalPos += horizontalMovement;
+
+        currentHorizontalPos = currentHorizontalPos % map.ElementAt(0).Length;
+
+        currentVerticalPos += verticalMovement;
+    }
+
+    return numberOfTrees;
+}
+
+int oneToOne = GetNumberOfTreesAlongSlope(1, 1);
+int threeToOne = GetNumberOfTreesAlongSlope(3, 1);
+int fiveToOne = GetNumberOfTreesAlongSlope(5, 1);
+int sevenToOne = GetNumberOfTreesAlongSlope(7, 1);
+int oneTotwo = GetNumberOfTreesAlongSlope(1, 2);
+
+Console.WriteLine(oneToOne);
+Console.WriteLine(threeToOne);
+Console.WriteLine(fiveToOne);
+Console.WriteLine(oneTotwo);
+Console.WriteLine(sevenToOne);
+
+long result = oneToOne * threeToOne * fiveToOne * sevenToOne * oneTotwo;
+
+Console.WriteLine(result);
