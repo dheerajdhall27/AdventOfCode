974b00eac0bba8f2014c1bcf53fffa18ea5fb3d5
dheerajdhall <ddhall@predictiveindex.com>
2021-12-02 20:04:55 -0500

    Completed Day 1 of Advent of code 2021

---
+++ AdventOfCode2021/Day1-SonarSweep/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+var depths = File.ReadAllLines("input.txt");
+
+List<int> depthsList = depths.Select(x => Int32.Parse(x)).ToList();
+
+int numberOfTimesDepthIncreases = 0;
+
+// Solution for the first part
+for (int i = 1; i < depthsList.Count(); i++)
+{
+    if (depthsList[i] > depthsList[i - 1])
+    {
+        numberOfTimesDepthIncreases++;
+    }
+}
+
+Console.WriteLine(numberOfTimesDepthIncreases);
+
+
+int depthIncreaseThreeSum = 0;
+// Solution for the second part
+for (int i = 1; i < depthsList.Count() - 2; i++)
+{
+    int firstWindowSum = depthsList[i - 1] + depthsList[i] + depthsList[i + 1];
+    int secondWindowSum = depthsList[i] + depthsList[i + 1] + depthsList[i + 2];
+
+    if (secondWindowSum > firstWindowSum)
+    {
+        depthIncreaseThreeSum++;
+    }
+}
+
+Console.WriteLine(depthIncreaseThreeSum);
