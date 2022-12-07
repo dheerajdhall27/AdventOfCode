3afdead12559e34b92ee1865072d545e08dcd3d0
dheerajdhall27 <dheerajdhall2709@gmail.com>
2022-12-06 21:24:26 -0500

    Finished Day 6 of AOC 22

--- AdventOfCode2021/Day11-DumboOctopus/Program.cs
+++ AdventOfCode2021/Day11-DumboOctopus/Program.cs
@@ -1,1 +1,59 @@
-﻿
+﻿var octopusesRow = File.ReadAllLines("input.txt");
+
+var octopusGrid = new List<List<int>>();
+
+void BuildOctopusGrid()
+{
+    foreach(var octopusRow in octopusesRow)
+    {
+        var list = octopusesRow.Select(x => int.Parse(x)).ToList();
+
+        octopusGrid.Add(list);
+    }
+}
+
+
+int GetTotalFlashesInKIterationsFloodFill(int iterations)
+{
+    int flashes = 0;
+    for(int i = 0;i < iterations; i++)
+    {
+        for(int row = 0;row < octopusGrid.Count; row++)
+        {
+            for(int column = 0; column < octopusGrid[i].Count;column++)
+            {
+                int value = octopusGrid[row][column];
+
+                value += 1;
+                if(value == 10)
+                {
+                    octopusGrid[row][column] = 0;
+                    GetOctopusFlashesAroundCoordinates(row, column);
+                }
+                else
+                {
+                    octopusGrid[row][column] = value;
+                }
+            }
+        }
+    }
+
+    return flashes;
+}
+
+int GetOctopusFlashesAroundCoordinates(int x, int y)
+{
+    int flashes = 0;
+    var stack = new Stack<(int, int)>();
+
+    return flashes;
+}
+
+
+List<(int, int)> GetAllNeighborsOfCoordinate(int x, int y)
+{
+
+}
+
+BuildOctopusGrid();
+Console.WriteLine(GetTotalFlashesInKIterationsFloodFill(100));
---
+++ AdventOfCode2022/Day6-TuningTrouble/Program.cs
@@ -0,0 +1,42 @@
+﻿var signal = File.ReadAllText("input.txt");
+
+int GetMarkerCharIndexAfterNthindex(int n)
+{
+    int markerIndex = -1;
+    var markerDictionary = new Dictionary<char, int>();
+
+    int index = 0;
+    foreach (var character in signal)
+    {
+
+        if(markerDictionary.ContainsKey(character))
+        {
+            int charIndex = markerDictionary[character];
+
+            var markerDictionaryKeys = markerDictionary.Keys;
+
+            foreach(var key in markerDictionaryKeys)
+            {
+                if (markerDictionary[key] <= charIndex)
+                {
+                    markerDictionary.Remove(key);
+                }
+            }
+        }
+
+        markerDictionary.Add(character, index);
+
+        if (markerDictionary.Count == n)
+        {
+            markerIndex = index + 1;
+            break;
+        }
+
+        index++;
+    }
+
+    return markerIndex;
+}
+
+Console.WriteLine(GetMarkerCharIndexAfterNthindex(4));
+Console.WriteLine(GetMarkerCharIndexAfterNthindex(14));
